31e27de6aad38670ad950674e557fd420a674bce
Daniel Bell <dchristianbell@gmail.com>
2013-06-06 15:46:03 -0700

    switched groupid

--- project.clj
+++ project.clj
@@ -1,15 +1,10 @@
-(defproject prismatic/dommy "0.1.2-SNAPSHOT"
+(defproject invaliduser/dommy "0.1.2-SNAPSHOT"
   :clojurescript? true
   :description "No nonsense Clojurescript dom templating and (soon) manipulation"
-  :url "https://github.com/prismatic/dommy"
+  :url "https://github.com/invaliduser/dommy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :scm {:url "git@github.com:prismatic/dommy.git"}
-  :pom-addition [:developers [:developer
-                              [:name "Prismatic"]
-                              [:url "http://getprismatic.com"]
-                              [:email "admin+oss@getprismatic.com"]
-                              [:timezone "-8"]]]
+  :scm {:url "git@github.com:invaliduser/dommy.git"}
   :plugins [[lein-cljsbuild "0.3.0"]]
   :hooks [leiningen.cljsbuild]
   :dependencies [[crate "0.2.3" :scope "dev"] ;; for perf test
